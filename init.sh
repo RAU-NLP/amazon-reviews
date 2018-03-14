44cb25bfc19535adc5f7ead01de447ff70bd2e5a
Adam B <adam@bittlingmayer.org>
2018-03-14 17:40:38 +0400

    Clone fastText, because it will be in the cache

--- init.sh
+++ init.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 if [ ! -f data/train.ft.txt ]; then
-    printf "\n\n PREPARING DATA...\n"
+    printf "\n\nPREPARING DATA...\n"
     cat data/train.chunk.* > data/train.ft.txt
     cat data/test.chunk.* > data/test.ft.txt
 fi
 
 if [ ! -f fastText/fasttext ]; then
     printf "\n\nBUILDING FASTTEXT...\n"
-    #git clone https://github.com/facebookresearch/fastText.git
+    git clone https://github.com/facebookresearch/fastText.git
     cd fastText
     make
     cd ..
