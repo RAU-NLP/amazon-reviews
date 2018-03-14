d34cc5625c9a2a3e6e98a9851ebb26bc6aec7581
Adam B <adam@bittlingmayer.org>
2018-03-14 17:27:29 +0400

    Cache data and fastText dirs across runs

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 printf "\n\nTRAINING MODEL...\n"
-./fastText/fasttext supervised -input data/train.ft.txt -output fastText/model
+./fastText/fasttext supervised -input data/train.ft.txt -output model
 
 printf "\n\nTESTING MODEL...\n"
-./fastText/fasttext test fastText/model.bin data/test.ft.txt
+./fastText/fasttext test model.bin data/test.ft.txt
