e017d34f9664114bb46559672675429ae6ab1d8c
Adam Mathias Bittlingmayer <bittlingmayer@users.noreply.github.com>
2018-03-18 16:05:49 +0400

    Add -verbose

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 printf "\n\nTRAINING MODEL...\n"
-./fastText/fasttext supervised -input data/train.ft.txt -output model
+./fastText/fasttext supervised -input data/train.ft.txt -output model -verbose
 
 printf "\n\nTESTING MODEL...\n"
 ./fastText/fasttext test model.bin data/test.ft.txt
